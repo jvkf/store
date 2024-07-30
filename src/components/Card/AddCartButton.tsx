1fbbbeb10190c059f43f8750cab9e9d05ea43ba4
João Ferraz (@p1padev) <git@p1pa.dev>
2024-07-30 17:04:59 -0300

    Update Card Component with controls

---
+++ src/components/Card/AddCartButton.tsx
@@ -0,0 +1,36 @@
+import { ShoppingCart } from 'lucide-react';
+import styled from 'styled-components';
+
+const AddBtn = styled.button`
+  border: 2px solid ${(props) => props.theme.colors.black};
+  appearance: none;
+  background: none;
+  display: inline-block;
+  width: 32px;
+  height: 32px;
+  aspect-ratio: 1;
+  border-radius: 100%;
+  cursor: pointer;
+  font-weight: 700;
+  transition: all 150ms ease-in;
+  flex-shrink: 0;
+
+  &:hover {
+    border-color: ${(props) => props.theme.colors.complementary};
+    background-color: ${(props) => props.theme.colors.complementary};
+    color: ${(props) => props.theme.colors.light};
+  }
+`;
+
+interface AddBtnProps {
+  onClick: () => void;
+}
+
+export default function AddCartButton({ onClick }: AddBtnProps) {
+  return (
+    <AddBtn onClick={onClick}>
+      <span className="sr-only">Adicionar</span>
+      <ShoppingCart strokeWidth={2.5} />
+    </AddBtn>
+  );
+}
--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,18 +1,26 @@
 import styled from 'styled-components';
 import type { Product } from '../../routes/store/useProductsQuery';
+import CartControls from './CartControls';
 
-const Information = styled.div`
+const ProductBar = styled.div`
   border-top: 1px solid ${(props) => props.theme.colors.black};
   display: flex;
   flex-direction: column;
   gap: 20px;
   padding: ${(props) => props.theme.spacing.small};
+  flex: auto;
 `;
 
 const CardContainer = styled.article`
   border-radius: 6px;
   border: 1px solid ${(props) => props.theme.colors.main};
-  box-shadow: 0 0 3px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 0 6px rgba(0, 0, 0, 0.3);
+  display: flex;
+  flex-direction: column;
+  transition: box-shadow 200ms ease-in-out;
+  &:hover {
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.4);
+  }
 `;
 
 const Image = styled.img`
@@ -30,19 +38,33 @@ const Title = styled.h2`
 const Price = styled.h3`
   font-size: 0.85rem;
   font-weight: 700;
+  padding-top: ${(props) => props.theme.spacing.small};
 `;
 
-export default function Card({ product }: { product: Product }) {
+const Information = styled.div`
+  flex: auto;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const formatCurrency = (value: number) =>
+  value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+
+interface CardProps {
+  product: Product;
+}
+
+export default function Card({ product }: CardProps) {
   return (
     <CardContainer>
       <Image src={product.image} alt={product.description} />
-      <Information>
-        <div>
+      <ProductBar>
+        <Information>
           <Title>{product.title}</Title>
-          <Price>{product.price}</Price>
-        </div>
-        {/* TODO: addbTn */}
-      </Information>
+          <Price>{formatCurrency(product.price)}</Price>
+        </Information>
+        <CartControls product={product} />
+      </ProductBar>
     </CardContainer>
   );
 }
