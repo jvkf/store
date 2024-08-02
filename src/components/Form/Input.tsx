eb14f030122785f5eb48649c2bca2d926e67d0a5
João Ferraz (@p1padev) <git@p1pa.dev>
2024-08-02 17:59:40 -0300

    Checkout form with Zod + RHF

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,48 @@
+import { forwardRef } from 'react';
+import { FieldErrors } from 'react-hook-form';
+import styled from 'styled-components';
+import { FormKeySchema, FormSchema } from './Schema';
+
+const StyledInput = styled.input`
+  appearance: none;
+  border: 1px solid black;
+  border-radius: 4px;
+  padding: 0.25rem;
+  font-size: 0.9rem;
+`;
+
+const ErrorMessage = styled.span`
+  min-height: 25px;
+  color: red;
+`;
+
+export type InputProps = {
+  registerProps: React.DetailedHTMLProps<
+    React.InputHTMLAttributes<HTMLInputElement>,
+    HTMLInputElement
+  >;
+  name: FormKeySchema;
+  label: string;
+  errors: FieldErrors<FormSchema>;
+  touched: boolean;
+  htmlProps?: React.InputHTMLAttributes<HTMLInputElement>;
+};
+
+const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ registerProps, name, label, errors, touched, htmlProps }, ref) => (
+    <>
+      <label htmlFor={name}>{label}</label>
+      <StyledInput
+        id={name}
+        aria-invalid={errors[name] ? 'true' : 'false'}
+        className={touched ? 'touched' : ''}
+        ref={ref}
+        {...registerProps}
+        {...htmlProps}
+      />
+      <ErrorMessage>{errors[name] && errors[name].message}</ErrorMessage>
+    </>
+  )
+);
+
+export default Input;
---
+++ src/components/Form/Schema.ts
@@ -0,0 +1,32 @@
+import { z } from 'zod';
+
+export const formSchema = z.object({
+  fullName: z
+    .string()
+    .min(1, { message: 'O nome completo é necessário ' })
+    .min(4, { message: 'O nome é muito curto, digite o nome completo' }),
+  ccNumber: z
+    .string()
+    .regex(/^[0-9]*$/, { message: 'Digite somente números' })
+    .min(16, { message: 'Digite os 16 dígitos' })
+    .transform((val) => parseInt(val)),
+  ccSecurity: z
+    .string()
+    .regex(/^[0-9]*$/, { message: 'Digite somente números' })
+    .min(3, { message: 'Digite os 3 dígitos' })
+    .transform((val) => parseInt(val)),
+  ccDate: z.coerce
+    .date({
+      message: 'Data inválida',
+      errorMap: (issue, { defaultError }) => ({
+        message: issue.code === 'invalid_date' ? 'Data inválida' : defaultError,
+      }),
+    })
+    .min(new Date(), {
+      message: 'A data de vencimento não pode já ter passado.',
+    }),
+});
+
+const _formKeys = formSchema.keyof();
+export type FormSchema = z.infer<typeof formSchema>;
+export type FormKeySchema = z.infer<typeof _formKeys>;
